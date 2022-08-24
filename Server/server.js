0c65453722ebbed524cf8cb380de0ecbd5309a59
Cristian Dao <cdao@cougarnet.uh.edu>
2022-08-24 14:40:39 -0500

    Creating endpoints for CRUD informations, and some filters.

--- Server/server.js
+++ Server/server.js
@@ -105,6 +105,170 @@ app.delete('/event/:id', (req, res, next) => {
     });
 });
 
+//create an endpoint to get all individuals information from the API
+app.get('/informations', (req, res, next) => {
+    //very plain way to get all the data from the collection through the mongoose schema
+    indiv_Info.find((error, data) => {
+        if (error) {
+            //here we are using a call to next() to send an error message back
+            return next(error)
+        } else {
+            res.json(data)
+        }
+    })
+});
+
+// endpoint for retrieving an information document by _id
+app.get('/information/:id', (req, res, next) => {
+    indiv_Info.findOne({ _id: req.params.id}, (error, data) => {
+        if (error) {
+            return next(error)
+        } else if (data === null) {
+            // Sending 404 when not found something is a good practice
+            res.status(404).send('Information not found');
+        }
+        else {
+            res.json(data)
+        }
+    });
+});
+
+//create an endpoint to know how many times and in what ways individuals have accessed events, individual's history
+app.get('/trackingInfos', (req, res, next) => {
+    //very plain way to get all the data from the collection through the mongoose schema
+    indiv_Info.aggregate([{
+            $group: {
+                _id: {firstName:'$firstName',
+                    lastName:'$lastName',
+                    phoneNumber: '$phoneNumber',
+                    zipCode: '$zipCode'
+                },
+                Count: {
+                    $sum: 1
+                },
+                socialMedia: {
+                    $addToSet: '$socialMedia'       // $addToSet returns an array of all unique values that results from applying an expression to each document in a group.
+                },
+                COVID19: {
+                    $addToSet: '$COVID19'
+                },
+                servicesNeeding: {
+                    $addToSet: '$servicesNeeding'
+                },
+                numOfChildren: {
+                    $addToSet: '$numOfChildren'
+                },
+                OverAge65: {
+                    $addToSet: '$OverAge65'
+                },
+                veteran: {
+                    $addToSet: '$veteran'
+                },
+                Ethnicity: {
+                    $addToSet: '$Ethnicity'
+                },
+                event: {
+                    $addToSet: '$event'
+                }
+            }}
+        ], (error, data) => {
+            if (error) {
+                //here we are using a call to next() to send an error message back
+                return next(error)
+            } else {
+                res.json(data)
+            }
+        })
+});
+
+//create an endpoint to know how many times and in what ways AN individual has accessed events, individual's history
+app.get('/trackingInfo/:firstName&:lastName', (req, res, next) => {
+    //very plain way to get all the data from the collection through the mongoose schema
+    indiv_Info.aggregate([{
+        $match: {                               // Using aggregation - match function to necessary data,  group them into 1 if there are documents that have the same data
+            firstName: req.params.firstName,
+            lastName: req.params.lastName
+        }
+    }, {
+        $group: {
+            _id: {
+                firstName: '$firstName',
+                lastName: '$lastName',
+                phoneNumber: '$phoneNumber',
+                zipCode: '$zipCode'
+            },
+            Count: {
+                $sum: 1
+            },
+            socialMedia: {
+                $addToSet: '$socialMedia'      // $addToSet returns an array of all unique values that results from applying an expression to each document in a group.
+            },
+            COVID19: {
+                $addToSet: '$COVID19'
+            },
+            servicesNeeding: {
+                $addToSet: '$servicesNeeding'
+            },
+            numOfChildren: {
+                $addToSet: '$numOfChildren'
+            },
+            OverAge65: {
+                $addToSet: '$OverAge65'
+            },
+            veteran: {
+                $addToSet: '$veteran'
+            },
+            Ethnicity: {
+                $addToSet: '$Ethnicity'
+            },
+            event: {
+                $addToSet: '$event'
+            }
+        }}
+        ], (error, data) => {
+            if (error) {
+                //here we are using a call to next() to send an error message back
+                return next(error)
+            } else {
+                res.json(data)
+            }
+        })
+});
+
+// Supervisor should be able to filter the data by zip code
+app.get('/zipcodeTracking/:zipcode', (req, res, next) => {
+    indiv_Info.aggregate([{
+        $match: {                                  // Using aggregation to match necessary data,  group them into 1 if there are documents that have the same data
+            zipCode: parseInt(req.params.zipcode)
+
+        }
+    }, {
+        $group: {
+            _id: {
+                firstName: '$firstName',
+                lastName: '$lastName',
+                phoneNumber: '$phoneNumber',
+                zipCode: '$zipCode'
+            }
+        }
+    }, {
+        $project: {                                // make the data that I want to display
+            _id: {
+                firstName: 1,
+                lastName: 1
+            }
+        }
+    }], (error, data) => {
+            if (error) {
+                //here we are using a call to next() to send an error message back
+                return next(error)
+            } else {
+                console.log(req.params.zipcode)
+                res.json(data)
+            }
+        })
+});
+
 app.listen(PORT, () => {
     console.log(`Server started listening at http://localhost:${PORT}`);
 });
